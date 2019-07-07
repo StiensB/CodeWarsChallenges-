b98b1feece38a1fb42ec2e377ab5e4ff15f5abda
Your Name <stiensb1@xavier.edu>
2019-07-07 16:05:09 -0400

    python daily challenge

--- square_digts.py
+++ square_digts.py
@@ -1,5 +1,6 @@
 #Bray Stiens
 #Sqaure the indiviual digits within a given integer
+#For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 def square_digits(num):
     temp = 0
     arr = []
